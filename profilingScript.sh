73a51b20d4828abccc52a0dc7ba37473fb35cc01
Brian Curless <bcurl3ss@gmail.com>
2024-04-02 10:24:55 -0700

    Changed profiling script to run released code
    This code is optimized and would be what we want to profile. Also
    updated modes to match my new mode numbering scheme.

--- profilingScript.sh
+++ profilingScript.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #
 # -f overwrite profile if it exists
-ncu -f  --app-replay-buffer=file --replay-mode=application -o "Mode 4" --clock-control=none --set full "debug/main" /data/expo/expo_16D_262144.txt 0.1 4
-ncu -f  --app-replay-buffer=file --replay-mode=application -o "Mode 42" --clock-control=none --set full "debug/main" /data/expo/expo_16D_262144.txt 0.1 42
-ncu -f  --app-replay-buffer=file --replay-mode=application -o "Mode 43" --clock-control=none --set full "debug/main" /data/expo/expo_16D_262144.txt 0.1 43
-ncu -f  --app-replay-buffer=file --replay-mode=application -o "Mode 44" --clock-control=none --set full "debug/main" /data/expo/expo_16D_262144.txt 0.1 44
+ncu -f  --app-replay-buffer=file --replay-mode=application -o "Mode 4" --clock-control=none --set full "release/main" /data/expo/expo_16D_2000000.txt 0.1 4
+ncu -f  --app-replay-buffer=file --replay-mode=application -o "Mode 41" --clock-control=none --set full "release/main" /data/expo/expo_16D_2000000.txt 0.1 41
+ncu -f  --app-replay-buffer=file --replay-mode=application -o "Mode 42" --clock-control=none --set full "release/main" /data/expo/expo_16D_2000000.txt 0.1 42
+ncu -f  --app-replay-buffer=file --replay-mode=application -o "Mode 43" --clock-control=none --set full "release/main" /data/expo/expo_16D_2000000.txt 0.1 43
