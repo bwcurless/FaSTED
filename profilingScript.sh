34d7c71b82040af89103400f883e0c857577cc2d
Brian Curless <bcurl3ss@gmail.com>
2024-05-13 21:00:52 -0700

    Profiled 16x16x16 short circuitable Mode44

--- profilingScript.sh
+++ profilingScript.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 #
+# -replay-mode=application, so it doesn't replay kernels individually. I believe there is not enough global memory on the GPU, we hang. It tries to just replay certain kernels over and over, but you get bad results if you do that because some kernels can't be repeated without rerunning all of them. application option means the entire application will be restarted when you profile, this guarantees there will be no errors while executing, but is slower to profile.
 # -f overwrite profile if it exists
-ncu -f  --app-replay-buffer=file --replay-mode=application -o "Mode 4" --clock-control=none --set full "release/main" /data/expo/expo_16D_2000000.txt 0.1 4
-ncu -f  --app-replay-buffer=file --replay-mode=application -o "Mode 41" --clock-control=none --set full "release/main" /data/expo/expo_16D_2000000.txt 0.1 41
-ncu -f  --app-replay-buffer=file --replay-mode=application -o "Mode 42" --clock-control=none --set full "release/main" /data/expo/expo_16D_2000000.txt 0.1 42
-ncu -f  --app-replay-buffer=file --replay-mode=application -o "Mode 43" --clock-control=none --set full "release/main" /data/expo/expo_16D_2000000.txt 0.1 43
+#ncu -f  --app-replay-buffer=file --replay-mode=application -o "Mode 4" --clock-control=none --set full "release/main" ~/datasets/expo_16D_200000.txt 0.1 4
+#ncu -f  --app-replay-buffer=file --replay-mode=application -o "mode 41" --clock-control=none --set full "release/main" ~/datasets/expo_16D_200000.txt 0.1 41
+#ncu -f  --app-replay-buffer=file --replay-mode=application -o "Mode 42" --clock-control=none --set full "release/main" ~/datasets/expo_16D_200000.txt 0.1 42
+#ncu -f  --app-replay-buffer=file --replay-mode=application -o "Mode 43" --clock-control=none --set full "release/main" ~/datasets/expo_16D_200000.txt 0.1 43
+ncu -f  --app-replay-buffer=file --replay-mode=application -o "Mode 44" --clock-control=none --set full "release/main" /data/expo/expo_16D_262144.txt 0.1 44
+#ncu -f  --app-replay-buffer=file --replay-mode=application -o "Mode 45" --clock-control=none --set full "release/main" ~/datasets/expo_16D_200000.txt 0.1 45
+#ncu -f  --app-replay-buffer=file --replay-mode=application -o "Mode 46" --clock-control=none --set full "release/main" ~/datasets/expo_16D_200000.txt 0.1 46
