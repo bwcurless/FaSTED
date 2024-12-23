04149dec092d7836a56491d321636c9059a7f93d
Brian Curless <bcurl3ss@gmail.com>
2024-12-23 10:04:41 -0700

    Data is being plotted now.
    Wrote script to read json, convert to a dataframe, average iterations, and plot the results.

--- results/scripts/plotResults.py
+++ results/scripts/plotResults.py
@@ -1,7 +1,64 @@
-import pickle
-from Experiment import Experiment
+import json
+import pandas as pd
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
-results = {}
-with open("selectivityVsSpeed.pkl", "rb") as f:
-    results = pickle.load(f)
-print(results)
+# Map out column names for later
+selCol = "selectivity"
+speedCol = "results.TFLOPS"
+inputSizeCol = "results.inputProblemShape.m"
+inputDimCol = "results.inputProblemShape.k"
+
+
+def parseSelectivityVsSpeedData():
+    with open("../selectivityVsSpeed.json", "r") as f:
+        selec_speed_results = json.load(f)
+    # Pretty print json
+    # print(json.dumps(selec_speed_results, indent=4))
+
+    df = pd.json_normalize(selec_speed_results)
+    # Average results from each iteration
+    averagedResults = (
+        df.groupby(selCol, as_index=False).mean().drop(columns=["iteration"])
+    )
+    print(averagedResults)
+    plt.figure(1)
+    plt.plot(averagedResults[selCol], averagedResults[speedCol], marker=".")
+    plt.xlabel("Selectivity")
+    plt.ylabel("TFLOPS")
+    plt.title("Selectivity vs Throughput")
+    plt.savefig("selectivityVsSpeed.png")
+    plt.show()
+
+
+def parseSpeedVsSizeData():
+    with open("../ExpoDataSpeedVsSize.json", "r") as f:
+        speed_size_results = json.load(f)
+    df = pd.json_normalize(speed_size_results)
+    # Average results from each iteration. We used a fixed selectity here, so groupBy input shape.
+    averagedResults = (
+        df.groupby([inputSizeCol, inputDimCol], as_index=False)
+        .mean()
+        .drop(columns=["iteration"])
+    )
+    print(averagedResults)
+    fig = plt.figure(2)
+    ax = fig.add_subplot(111, projection="3d")
+    surf = ax.plot_trisurf(
+        averagedResults[inputSizeCol],
+        averagedResults[inputDimCol],
+        averagedResults[speedCol],
+        cmap="viridis",
+    )
+    ax.set_xlabel("Dataset Size (Points)")
+    ax.set_ylabel("Dataset Dimensionality")
+    ax.set_zlabel("Speed (TFLOPS)")
+    ax.set_title("Speed vs Dataset Shape")
+    fig.colorbar(surf)
+    plt.savefig("ExpoDataSpeedVsSize.png")
+    plt.show()
+
+
+if __name__ == "__main__":
+    parseSelectivityVsSpeedData()
+    parseSpeedVsSizeData()
