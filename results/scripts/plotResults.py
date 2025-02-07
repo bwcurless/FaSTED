48d5a0d1ec752c24f6bc3ec2e9f438adec9894d4
Brian Curless <bcurl3ss@gmail.com>
2025-02-07 10:31:30 +0100

    Updated figures.
    - Updated real world data to use actual totalTime property instead of backcalculating it.
    - Added label for maximium y value.
    - Removed plot titles
    - Labeled datasets below plots
    - Adjusted font sizes

--- results/scripts/plotResults.py
+++ results/scripts/plotResults.py
@@ -18,7 +18,7 @@ SPEED_COL = "results.TFLOPS"
 INPUT_SIZE_COL_M = "results.inputProblemShape.m"
 INPUT_SIZE_COL_N = "results.inputProblemShape.n"
 INPUT_DIM_COL = "results.inputProblemShape.k"
-TIME = "totalTime"
+TIME = "results.totalTime"
 ALGORITHM = "algorithm"
 DATASET_NAME = "dataset"
 
@@ -46,10 +46,9 @@ def parse_selectivity_vs_speed_data():
     plt.plot(
         averaged_results[SEL_COL], averaged_results[SPEED_COL], marker="."
     )
-    plt.xlabel("Selectivity")
+    plt.xlabel("Selectivity Level")
     plt.ylabel("TFLOPS")
     plt.ylim(0, 300)
-    plt.title("Selectivity vs Throughput")
     plt.savefig("selectivityVsSpeed.pdf")
     plt.show()
 
@@ -98,6 +97,8 @@ def parse_speed_vs_size_data():
         cmap="viridis",
         origin="lower",
         aspect="auto",
+        vmin=0,
+        vmax=160,
         extent=(
             0,
             num_dims_tested,
@@ -109,11 +110,11 @@ def parse_speed_vs_size_data():
     # To center labels on pixels, must offset by a half a pixel
     center_offset = 0.5
 
-    ax.set_xlabel("Dataset Dimensionlity")
+    ax.set_xlabel("Dataset Dimensionlity ($d$)")
     ax.set_xticks(np.arange(num_dims_tested) + center_offset)
     ax.set_xticklabels(unique_dim)
 
-    ax.set_ylabel("Dataset Size (Points)")
+    ax.set_ylabel("Dataset Size ($|D|$)")
     ax.set_yticks(np.arange(num_sizes_tested) + center_offset)
     ax.set_yticklabels(unique_size)
 
@@ -132,8 +133,13 @@ def parse_speed_vs_size_data():
             )
 
     colorbar = fig.colorbar(cax, ax=ax)
-    colorbar.set_label("Speed (TFLOPS)")
-    ax.set_title("Algorithm Performance")
+    colorbar.set_label("Throughput (TFLOPS)")
+
+    # Get the current ticks (positions) of the colorbar
+    ticks = colorbar.get_ticks()
+    colorbar.set_ticks(ticks)
+    colorbar.set_ticklabels([f'{tick:.2f}' for tick in ticks])
+
     plt.tight_layout()
     # plt.savefig("ExpoDataSpeedVsSize.pdf")
     plt.show()
@@ -157,14 +163,14 @@ def plot_real_world_data_speed_comparison():
             .mean()
             .drop(columns=["iteration"])
         )
-        # I never saved the total time, so compute it backwards.
-        averaged_results[TIME] = (
-            averaged_results[INPUT_SIZE_COL_M]
-            * averaged_results[INPUT_SIZE_COL_N]
-            * averaged_results[INPUT_DIM_COL]
-            * 2
-            / (averaged_results[SPEED_COL] * 10**12)
-        )
+        ## I originally never saved the total time, so compute it backwards.
+        #averaged_results[TIME] = (
+        #    averaged_results[INPUT_SIZE_COL_M]
+        #    * averaged_results[INPUT_SIZE_COL_N]
+        #    * averaged_results[INPUT_DIM_COL]
+        #    * 2
+        #    / (averaged_results[SPEED_COL] * 10**12)
+        #)
         averaged_results[DATASET_NAME] = dataset_name
         # Mark these results as my own
         averaged_results[ALGORITHM] = MPTC_JOIN
@@ -177,27 +183,28 @@ def plot_real_world_data_speed_comparison():
         return minimal_results
 
     # Assemble all mptc results
-    datasets = [
-        "cifar60k (512D)",
-        "tiny5m (384D)",
-        "sift10m (128D)",
-        "gist1m (960D)",
+    dataset_labels = [
+        "cifar60k",
+        "tiny5m",
+        "sift10m",
+        "gist1m",
     ]
-    cifar = datasets[0]
-    tiny = datasets[1]
-    sift = datasets[2]
-    gist = datasets[3]
+    cifar = dataset_labels[0]
+    tiny = dataset_labels[1]
+    sift = dataset_labels[2]
+    gist = dataset_labels[3]
 
     mptc_files = [
-        "cifar60k_unscaled.txt_results_1736444128.json",
-        "tiny5m_unscaled.txt_results_1736399317.json",
-        "sift10m_unscaled.txt_results_1736663134.json",
-        "gist_unscaled.txt_results_1736483286.json",
+	    "cifar60k_unscaled.txt_results_1737956025.json",
+	    "gist_unscaled.txt_results_1738039369.json",
+	    "sift10m_unscaled.txt_results_1738048428.json",
+	    "tiny5m_unscaled.txt_results_1737960896.json",
     ]
-    mptc_results = pd.DataFrame()
-    for file, dataset_name in zip(mptc_files, datasets):
-        mptc_results = pd.concat(
-            [mptc_results, extract_mptc_results(file, dataset_name)],
+
+    all_results = pd.DataFrame()
+    for file, dataset_name in zip(mptc_files, dataset_labels):
+        all_results = pd.concat(
+            [all_results, extract_mptc_results(file, dataset_name)],
             ignore_index=True,
         )
 
@@ -217,33 +224,33 @@ def plot_real_world_data_speed_comparison():
         }
         df.loc[len(df)] = new_row
 
-    add_row(mptc_results, GDS_JOIN, cifar, 2.329744667, 64)
-    add_row(mptc_results, GDS_JOIN, cifar, 2.675126, 128)
-    add_row(mptc_results, GDS_JOIN, cifar, 3.067051667, 256)
+    add_row(all_results, GDS_JOIN, cifar, 2.329744667, 64)
+    add_row(all_results, GDS_JOIN, cifar, 2.675126, 128)
+    add_row(all_results, GDS_JOIN, cifar, 3.067051667, 256)
 
-    add_row(mptc_results, GDS_JOIN, tiny, 670.0995993, 64)
-    add_row(mptc_results, GDS_JOIN, tiny, 835.6346603, 128)
-    add_row(mptc_results, GDS_JOIN, tiny, 1056.39112, 256)
+    add_row(all_results, GDS_JOIN, tiny, 670.0995993, 64)
+    add_row(all_results, GDS_JOIN, tiny, 835.6346603, 128)
+    add_row(all_results, GDS_JOIN, tiny, 1056.39112, 256)
 
-    add_row(mptc_results, GDS_JOIN, sift, 3392.178252, 64)
-    add_row(mptc_results, GDS_JOIN, sift, 4279.934975, 128)
-    add_row(mptc_results, GDS_JOIN, sift, 5195.027983, 256)
+    add_row(all_results, GDS_JOIN, sift, 3392.178252, 64)
+    add_row(all_results, GDS_JOIN, sift, 4279.934975, 128)
+    add_row(all_results, GDS_JOIN, sift, 5195.027983, 256)
 
-    add_row(mptc_results, GDS_JOIN, gist, 272.1477887, 64)
-    add_row(mptc_results, GDS_JOIN, gist, 359.6358573, 128)
-    add_row(mptc_results, GDS_JOIN, gist, 444.5647683, 256)
+    add_row(all_results, GDS_JOIN, gist, 272.1477887, 64)
+    add_row(all_results, GDS_JOIN, gist, 359.6358573, 128)
+    add_row(all_results, GDS_JOIN, gist, 444.5647683, 256)
 
-    print(mptc_results)
+    print(all_results)
 
-    # Assemble all ted-join results
+    # TODO Assemble all ted-join results
 
     # Create the figure
     algorithms = [MPTC_JOIN, GDS_JOIN]
 
     # Create 4 subplots, one for each dataset
-    plot_rows = 2
-    plot_cols = 2
-    fig, ax = plt.subplots(plot_rows, plot_cols, layout="constrained")
+    plot_rows = 1
+    plot_cols = 4
+    fig, ax = plt.subplots(plot_rows, plot_cols, layout="constrained", figsize=(10, 3))
 
     # Choose a colormap (e.g., "viridis", "plasma", "Greys", etc.)
     colormap = cm.get_cmap("Greys")
@@ -255,9 +262,26 @@ def plot_real_world_data_speed_comparison():
 
     # Collect all handles and labels from each axis to create only one legend
     handles, labels = [], []
+
+    # Hardcode y limits specific to data for better readability
+    y_limits = [
+        4,
+        1250,
+        6000,
+        500,
+    ]
+
+    figure_label_prefixes = [
+        '(a)',
+        '(b)',
+        '(c)',
+        '(d)',
+    ]
+
+    # Create all subplots
     for i, axis in enumerate(ax.flat):
-        dataset = datasets[i]
-        single_dataset = mptc_results[mptc_results[DATASET_NAME] == dataset]
+        dataset_name = dataset_labels[i]
+        single_dataset = all_results[all_results[DATASET_NAME] == dataset_name]
         single_dataset_minimal_columns = single_dataset[
             [ALGORITHM, TIME, SEL_COL]
         ].sort_values(by=SEL_COL, ascending=True)
@@ -278,6 +302,7 @@ def plot_real_world_data_speed_comparison():
             by=ALGORITHM, ascending=False
         )
 
+
         x = np.arange(len(selectivities))  # the label locations
         width = 0.25  # the width of the bars
         multiplier = 0
@@ -296,10 +321,19 @@ def plot_real_world_data_speed_comparison():
             multiplier += 1
 
         # Add some text for labels, title and custom x-axis tick labels, etc.
-        axis.set_ylabel("Time (Seconds)")
-        axis.set_title(dataset)
+        axis.set_axisbelow(True)
+        axis.grid(axis="y")
+        axis.set_ylim(0, y_limits[i])
+        axis_label_fontsize = 9
+        # Only label first y axis to reduce clutter
+        if(i == 0):
+            axis.set_ylabel("Time (s)", fontsize=axis_label_fontsize)
+        figure_label = f"{figure_label_prefixes[i]} {dataset_name}"
+        axis.set_title(figure_label, y=-0.5, fontsize=8)
         axis.set_xticks(x + width, selectivities)
-        axis.set_xlabel("Selectivity")
+        axis.set_xlabel("Selectivity", fontsize=axis_label_fontsize)
+        # Decrease size of tick labels
+        axis.tick_params(axis='both', labelsize=axis_label_fontsize - 1)
 
         # Save this axis's labels and handles for the high level legend
         h, l = axis.get_legend_handles_labels()
@@ -325,5 +359,5 @@ def plot_real_world_data_speed_comparison():
 
 if __name__ == "__main__":
     # parse_selectivity_vs_speed_data()
-    parse_speed_vs_size_data()
-    # plot_real_world_data_speed_comparison()
+    # parse_speed_vs_size_data()
+    plot_real_world_data_speed_comparison()
