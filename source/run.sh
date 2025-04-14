3edbd63e709aab0504a539e32c14e6daca988d50
Brian Curless <bcurl3ss@gmail.com>
2025-04-13 22:43:44 -0700

    Reverted run.sh to simpler sbatch version.
    I wasn't really getting any benefits from my more convoluted scripts
    since I only run on one target now.

--- source/run.sh
+++ source/run.sh
@@ -1,48 +1,14 @@
 #!/bin/bash
-# Script to compile .cu files and run sbatch
-
-# You will need to copy these helper routines to this location to run this script.
-source ~/helpers/helper_slurm_funcs.sh
-
-# Exit if any part fails
-set -e
-
-# Input Arguments
-jobName=$1
-CC=80
-gpu=a100
-
-# Add a dash on if we are customizing the filename
-if [[ -n $jobName ]]; then
-jobPrefix=$jobName-
-fi
-
-outputFile="MMAPTXTest"
-
-# Do a test build locally to make sure there aren't errors before waiting in queue
-#echo "Building executable to $outputFile"
-#module load cuda
-#make clean
-#make
-
-# Define where outputs go
-outputPath="/scratch/$USER/"
-errorPath="$outputPath"
-
-echo "Executing..."
-
-jobid=$(sbatch --parsable <<SHELL
-#!/bin/bash
-#SBATCH --job-name=$jobPrefix$outputFile  #the name of your job
+#SBATCH --job-name=cuSimSearch  #the name of your job
 
 #change to your NAU ID below
-#SBATCH --output=$outputPath$jobPrefix$outputFile-%j.out
-#SBATCH --error=$errorPath$jobPrefix$outputFile-%j.out
+#SBATCH --output=/scratch/bc2497/cuSimSearch-%j.out
+#SBATCH --error=/scratch/bc2497/cuSimSearch-%j.out
 
 #SBATCH --time=01:00:00
 #SBATCH --mem=128000         #memory requested in MiB
 #SBATCH -G 1 #resource requirement (1 GPU)
-#SBATCH -C $gpu #GPU Model: k80, p100, v100, a100
+#SBATCH -C a100 #GPU Model: k80, p100, v100, a100
 
 # Gowanlock Partition
 #SBATCH --account=gowanlock_condo
@@ -76,10 +42,3 @@ srun ./release/main -e  100000 4096 0.001
 
 
 echo "----------------- JOB FINISHED -------------"
-
-SHELL
-)
-
-
-#waitForJobComplete "$jobid"
-#printFile "$outputPath$jobPrefix$outputFile-$jobid.out"
